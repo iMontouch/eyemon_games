cd75260ec80a4ed7c579f367a46ff804d139527d
imontouch <imontouch@gmail.com>
2020-03-19 13:25:51 +0100

    added games

---
+++ minecraft/eternal/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+cd /data
+
+cp -rf /tmp/feed-the-beast/* .
+echo "eula=true" > eula.txt
+
+if [[ ! -e server.properties ]]; then
+    cp /tmp/server.properties .
+fi
+
+if [[ -n "$MOTD" ]]; then
+    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [[ -n "$LEVEL" ]]; then
+    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+fi
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' >> ops.txt
+fi
+
+java -Xms2G -Xmx6G -XX:PermSize=256m -Dfml.read.Timeout=560 -jar forge-*-universal.jar nogui
