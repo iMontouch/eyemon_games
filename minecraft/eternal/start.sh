364b0b8761366c978a01891b14889c5ad7b8029f
iMontouch <simonklimke@web.de>
2020-04-05 13:24:33 +0200

    Update start.sh

--- minecraft/eternal/start.sh
+++ minecraft/eternal/start.sh
@@ -21,4 +21,4 @@ if [[ -n "$OPS" ]]; then
     echo $OPS | awk -v RS=, '{print}' >> ops.txt
 fi
 
-java -Xms8G -Xmx12G -XX:PermSize=256m -d64 -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:MaxTenuringThreshold=15 -XX:MaxGCPauseMillis=30 -XX:-UseGCOverheadLimit -XX:+UseBiasedLocking -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:+AggressiveOpts -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:SoftRefLRUPolicyMSPerMB=20000 -XX:ParallelGCThreads=4 -Dfml.read.Timeout=560 -jar forge-*-universal.jar nogui
+java -Xms8G -Xmx12G -XX:PermSize=256m -d64 -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:MaxTenuringThreshold=15 -XX:MaxGCPauseMillis=30 -XX:-UseGCOverheadLimit -XX:+UseBiasedLocking -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:+AggressiveOpts -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:SoftRefLRUPolicyMSPerMB=20000 -XX:ParallelGCThreads=4 -Dfml.read.Timeout=560 -Dfml.queryResult=confirm -jar forge-*-universal.jar nogui
