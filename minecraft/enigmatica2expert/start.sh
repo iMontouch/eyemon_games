82a3767884798102185e4656d00b84db323a30fa
imontouch <imontouch@gmail.com>
2020-04-25 21:56:28 +0200

    added e2e

---
+++ minecraft/enigmatica2expert/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+cd /data
+
+cp -rf /tmp/feed-the-beast/* .
+echo "eula=true" > eula.txt
+
+if [[ -n "$MOTD" ]]; then
+    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [[ -n "$LEVEL" ]]; then
+    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+fi
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' >> ops.txt
+fi
+
+/bin/bash ServerStartLinux.sh
