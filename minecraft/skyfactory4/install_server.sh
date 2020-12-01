f06931b2209ec0d9cd90fc89157779a191288c6e
Simon Klimke <simon.klimke@m2dot.com>
2020-12-01 20:22:16 +0100

    configured sf4 server.properties and docker.yml

---
+++ minecraft/skyfactory4/install_server.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo mkdir /opt/minecraft/sf4
+sudo chown 845:845 /opt/minecraft/sf4
+cp server.properties /opt/minecraft/sf4/server.properties
+cp whitelist.json /opt/minecraft/sf4/whitelist.json
+cp ops.json /opt/minecraft/sf4/ops.json
+
+docker-compose up -d
