ae7849c335c7cba10f97f68696f99e3be6860710
Simon Klimke <simon.klimke@m2dot.com>
2020-12-01 20:54:09 +0100

    added settings.sh to change used ram in sf4

--- minecraft/skyfactory4/install_server.sh
+++ minecraft/skyfactory4/install_server.sh
@@ -2,6 +2,7 @@
 sudo mkdir /opt/minecraft/sf4
 sudo chown 845:845 /opt/minecraft/sf4
 cp server.properties /opt/minecraft/sf4/server.properties
+cp settings.sh /opt/minecraft/sf4/settings.sh
 cp whitelist.json /opt/minecraft/sf4/whitelist.json
 cp ops.json /opt/minecraft/sf4/ops.json
 
