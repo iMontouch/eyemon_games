8861d255506b323c84a632c9602ecb1b086dd4e5
Simon Klimke <simon.klimke@m2dot.com>
2020-12-02 19:22:42 +0100

    fix

--- minecraft/enigmatica2normal/start.sh
+++ minecraft/enigmatica2normal/start.sh
@@ -6,16 +6,17 @@ cd /data
 
 #cp -rf /data/config /tmp/feed-the-beast/config
 #cp -rf /tmp/feed-the-beast/* .
-echo "eula=true" > eula.txt
 
-if [[ -n "$MOTD" ]]; then
-    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
-fi
-if [[ -n "$LEVEL" ]]; then
-    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
-fi
-if [[ -n "$OPS" ]]; then
-    echo $OPS | awk -v RS=, '{print}' >> ops.txt
-fi
-
-/bin/bash ServerStartLinux.sh
+#echo "eula=true" > /data/eula.txt
+#
+#if [[ -n "$MOTD" ]]; then
+#    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+#fi
+#if [[ -n "$LEVEL" ]]; then
+#    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+#fi
+#if [[ -n "$OPS" ]]; then
+#    echo $OPS | awk -v RS=, '{print}' >> ops.txt
+#fi
+#
+#/bin/bash ServerStartLinux.sh
