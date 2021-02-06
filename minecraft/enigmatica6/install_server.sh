ea7bbc23e211a3eba999762f1220c1085d75d1fb
imontouch <imontouch@gmail.com>
2021-02-06 20:33:45 +0100

    add enigmatica6 server

---
+++ minecraft/enigmatica6/install_server.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker build --tag enigmatica6:1.0 .
+sudo mkdir -p /opt/minecraft/e6
+# lazy shit
+sudo chown -R 777 /opt/minecraft/e6
+
+docker-compose up -d
---
+++ minecraft/enigmatica6/start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+cd /data
+
+#cp -rf /data/config /tmp/feed-the-beast/config
+if [ "$(ls -A $DIR)" ]; then
+    echo "Skipping initial Serverinstallation\nIt seems to be already installed"
+else
+  cp -rf /tmp/feed-the-beast/* .
+	echo "eula=true" > /data/eula.txt
+	sed -i 's/enable-rcon=false/enable-rcon=true/g' /data/server.properties
+	echo "rcon.port=25575" >> /data/server.properties
+	echo "rcon.password=1337" >> /data/server.properties
+	#wget https://media.forgecdn.net/files/3106/504/Dynmap-3.1-beta5-forge-1.12.2.jar -o /data/mods/Dynmap-3.1-beta5-forge-1.12.2.jar
+	#wget https://dl.dropboxusercontent.com/s/3fuqise4uee9rt7/spark-forge.jar -o /data/mods/spark-forge.jar
+fi
+
+#if [[ -n "$MOTD" ]]; then
+#	# buggy
+#    #sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+#fi
+#if [[ -n "$LEVEL" ]]; then
+#	# seems to be buggy
+#    #sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+#fi
+#if [[ -n "$OPS" ]]; then
+#	# ops.json is the way
+#    #echo $OPS | awk -v RS=, '{print}' >> ops.txt
+#fi
+
+/bin/bash server-start.sh
